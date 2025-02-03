f98beafacb2dadc041fbe87d0ae6c3787bcf6f0b
Chrisyaaan <christian@vontos.co>
2025-02-03 11:21:22 +0800

    user id changed

--- src/pages/Dashboard/components/MainViewTest.jsx
+++ src/pages/Dashboard/components/MainViewTest.jsx
@@ -24,7 +24,9 @@ const MainViewTest = () => {
 
   const isLoading = false;
 
-  const userId = localStorage.getItem("userId") || user.userId;
+  const userId =  user.userId;
+
+  console.log(userId)
 
   // console.log("ADMISSIONS FROM MAIN VIEW: ", admissions)
 
